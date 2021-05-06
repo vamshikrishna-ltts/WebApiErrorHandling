59e0ae325be4096fd8e778a2b31498876f7872ee
vamshikrishna-ltts <83207550+vamshikrishna-ltts@users.noreply.github.com>
2021-05-06 14:52:41 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,8 @@ namespace Analyzers
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+              services.AddControllers(options =>
+    options.Filters.Add(new HttpResponseExceptionFilter()));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
